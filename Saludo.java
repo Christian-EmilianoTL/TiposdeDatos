cdf781dbcf9e4fe01ee59b37037e9c6fd69d5eb5
Christian-EmilianoTL <20253rd026@utez.edu.mx>
2026-02-24 21:22:55 -0600

    Declaramos variables y metemos una nueva libreria

--- Saludo.java
+++ Saludo.java
@@ -1,5 +1,12 @@
+import java.util.Scanner; 
 public class Saludo{
     public static void main (String [] args){
+        Scanner ingreso = new Scanner(System.in); 
+        String nombre;
+        int edad; 
+
+        System.out.println("Dime tu nombre:");
         
+
     }
 }
